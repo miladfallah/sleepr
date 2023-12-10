d3da5e7225837f2d1428d600d4bc07cb9774e19e
MiladFallah <falahmilad79@gmail.com>
2023-12-10 16:27:13 +0330

    Initial commit

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { CurrentUser } from '../../../../libs/common/src/decorators/current-user.decorator';
+import { UserDocument } from './models/user.schema';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getUser(@CurrentUser() user: UserDocument) {
+    return user;
+  }
+}
---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,35 @@
+import { ReservationRepository } from './reservations.repository';
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+
+@Injectable()
+export class ReservationsService {
+  constructor(private readonly reservationRepository: ReservationRepository) {}
+  create(createReservationDto: CreateReservationDto, userId: string) {
+    return this.reservationRepository.create({
+      ...createReservationDto,
+      timeStamp: new Date(),
+      userId,
+    });
+  }
+
+  findAll() {
+    return this.reservationRepository.find({});
+  }
+
+  findOne(_id: string) {
+    return this.reservationRepository.findOne({ _id });
+  }
+
+  update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservationRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  remove(_id: string) {
+    return this.reservationRepository.findOneAndDelete({ _id });
+  }
+}
